973fe376366cd4d42c1df760567c908b12756b33
RikenKapadia <rkapadia@mail.sfsu.edu>
2025-06-15 19:10:44 -0700

    Completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,37 @@
+# Time complexity:
+# push(): O(1) - constant time to add node at the beginning
+# pop(): O(1) - constant time to remove node from the beginning
 
+# Space Complexity: 
+# O(n) - where n is the number of elements in the stack.
+
+# Node class to represent each element in the stack.
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
+       self.data = data # top of the stack
+       self.next = None # pointer to the next node
  
 class Stack:
     def __init__(self):
+        self.head = None # top of the stack
+        self.length = 0 # Length of the stack.
         
     def push(self, data):
+        new_node = Node(data)   # Create a new node 
+        new_node.next = self.head # New node to current head
+        self.head = new_node    # Update head with new node.
+        self.length += 1    # increase stack size
+        return self
         
     def pop(self):
+        if self.length==0: # IF stack is empty returning None.
+            return None
+        temp = self.head    # store current top
+        self.head = self.head.next  # move head to next node
+        self.length -= 1    # decrease stack size
+        return temp.data    # return popped data(value)
         
+    
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,18 @@
+# Time Complexity: 
+# append: O(n)
+# find: O(n)
+# remove: O(n)
+
+# Space Complexity: 
+# O(n) – where n is the number of nodes in the list
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data=data
+        self.next=next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,12 +21,23 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.length=0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node=ListNode(data) # Create a new node
+        if self.length==0: # If the list is empty, set head to new node
+            self.head=new_node
+        else:
+            temp=self.head # Traverse to the end of the list
+            while temp.next:
+                temp=temp.next
+            temp.next=new_node  # Set last node's next to new node
+        self.length+=1 # Increase the length.
+        
         
     def find(self, key):
         """
@@ -24,9 +45,45 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp=self.head
+        while temp:
+            if temp.data==key:
+                return temp # Found the matching node
+            temp=temp.next # Move to the next node
+        return None # Key not found
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        temp=self.head
+        pre=None # To keep track of previous node
+
+        while temp:
+            if temp.data==key:
+                if pre:
+                    pre.next=temp.next # If it's not the head, skip over the node to remove it
+                else:
+                    self.head=temp.next # If it's the head node, update head to next node
+                return True # Node found and removed
+            pre=temp # Move previous pointer
+            temp=temp.next # Move current pointer
+        return False # Key not found in list
+    
+
+
+sll = SinglyLinkedList()
+sll.append(1)
+sll.append(2)
+sll.append(3)
+
+found = sll.find(2)
+print("Found:", found.data if found else "Not found")
+
+removed = sll.remove(2)
+print("Removed:", removed)
+
+found_again = sll.find(2)
+print("Found after removal:", found_again.data if found_again else "Not found")
